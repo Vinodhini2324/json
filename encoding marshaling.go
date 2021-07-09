3fe46b65e9066573d053db94101e40e2aec0428f
Vinodhini2324 <86877127+Vinodhini2324@users.noreply.github.com>
2021-07-09 11:11:44 +0530

    encoding marshaling.go

---
+++ encoding marshaling.go
@@ -0,0 +1,50 @@
+# json
+package main
+   
+import (
+    "fmt"
+    "encoding/json"
+)
+   
+
+type Human struct{
+          
+    Name string
+    Age int
+    Address string
+}
+   
+func main() {
+       
+    
+    human1 := Human{"vinodhini", 22, "New Delhi"}
+         
+    human_enc, err := json.Marshal(human1)
+       
+    if err != nil {
+           
+        
+        fmt.Println(err)
+    }
+       
+    fmt.Println(string(human_enc))
+    
+    human2 := []Human{
+        {Name: "vinodhini", Age: 22, Address: "New Delhi"},
+        {Name: "enbam", Age: 27, Address: "Pune"},
+        {Name: "deepa", Age: 21, Address: "Bangalore"},
+    }
+       
+    
+    human2_enc, err := json.Marshal(human2)
+        
+        if err != nil {
+       
+        
+            fmt.Println(err)
+        }
+           
+    
+    fmt.Println()
+        fmt.Println(string(human2_enc))
+}
